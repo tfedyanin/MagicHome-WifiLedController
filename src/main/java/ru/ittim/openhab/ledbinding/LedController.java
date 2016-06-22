c8a3f8df0ec0fb4cd01555d00ef315f23058f728
tim06ka <fedyanint@gmail.com>
2016-06-22 22:54:31 +0300

    initial request / response

--- src/main/java/ru/ittim/openhab/ledbinding/LedController.java
+++ src/main/java/ru/ittim/openhab/ledbinding/LedController.java
@@ -1,18 +1,41 @@
 package ru.ittim.openhab.ledbinding;
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Бин, описывающий wifi-контроллер светодиодной ленты
  * Created by Timofey on 21.06.2016.
  */
 class LedController {
+    private final static int DEFAULT_CONTROLLER_PORT = 5577;
+    private final static Logger logger = LogManager.getLogger();
+
+    //сообщение контроллеру, на который приходит ответ длиной 14
+    private final static byte[] MSG = {(byte) 0x81, (byte) 0x8a, (byte) 0x8b, (byte) 0x96};
+
     private final String host;
     private final String mac;
     private final String model;
+    private static final int TIMEOUT = 150;
+
+    private Socket socket;
 
     LedController(String host, String mac, String model) {
         this.host = host;
         this.mac = mac;
         this.model = model;
+
+        try {
+            socket = new Socket(host, DEFAULT_CONTROLLER_PORT);
+            socket.setSoTimeout(TIMEOUT);
+        } catch (IOException e) {
+            logger.error("Не удалось создать подключение к контроллеру " + this, DEFAULT_CONTROLLER_PORT);
+        }
     }
 
     public String getHost() {
@@ -53,5 +76,38 @@ class LedController {
         result = 31 * result + mac.hashCode();
         result = 31 * result + model.hashCode();
         return result;
+    }
+
+    public void initialRequest() {
+        try {
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            DataOutputStream dos = new DataOutputStream(out);
+            dos.write(MSG);
+            byte[] bytes = new byte[14];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int len;
+            while ((len = in.read(bytes)) > -1) {
+                    baos.write(bytes, 0, len);
+                if (baos.size()>=14) {
+                    break;
+                }
+
+            }
+
+            if (baos.size() != 14) {
+                logger.error("Получен ответ некорректной длины");
+            }
+            System.out.println(Hex.encodeHexString(baos.toByteArray()));
+
+
+        } catch (IOException e) {
+            logger.error("Сокет не операбелен", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        LedController controller = new LedController("192.168.1.181", "ACCF239939B4", "HF-LPB100-ZJ200");
+        controller.initialRequest();
     }
 }
