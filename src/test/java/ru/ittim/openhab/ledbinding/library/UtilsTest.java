bc277a76fd17cdc710e978a407bfd9b05ed021a0
Fedyanin Timofey <fedyanint@gmail.com>
2018-12-23 22:47:05 +0300

    Add code coverage
    Add code coverage

---
+++ src/test/java/ru/ittim/openhab/ledbinding/library/UtilsTest.java
@@ -0,0 +1,21 @@
+package ru.ittim.openhab.ledbinding.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @Test
+    public void withCheckSumTest(){
+        byte[] initialArr = "Test".getBytes();
+        byte[] bytes = Utils.withCheckSum(initialArr);
+        initialArr = Arrays.copyOf(initialArr, initialArr.length + 1);
+        initialArr[initialArr.length-1] = -96;
+        assertArrayEquals(bytes, initialArr);
+    }
+
+
+}
